1e24e7459fa1dd53be07c602903679d836302ad6
Kippie <jenstheminer@gmail.com>
2024-01-28 15:30:46 +0100

    1.0.5

--- energai/CHANGELOG.md
+++ energai/CHANGELOG.md
@@ -1,3 +1,3 @@
-EnergAI 1.0.4
+EnergAI 1.0.5
 
 - Changed the webserver
--- energai/run.sh
+++ energai/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/with-contenv bashio
 
 echo "Hello world!"
-python3 -m http.server 8000 --directory web
+python3 -m http.server 8000
